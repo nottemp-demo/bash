109abd5cc07ef7b72bf9f2c9894b3f0263815780
nottemp-demo <148553657+nottemp-demo@users.noreply.github.com>
2023-10-21 09:55:08 +0300

    Update topsubs.sh

--- topsubs.sh
+++ topsubs.sh
@@ -6,6 +6,6 @@ current_dir=$(pwd)
 # List all subdirectories in the current directory and their sizes
 subdirectories=$(du -sh $current_dir/*/ 2>/dev/null | sort -rh)
 
-# Print the top 10 subdirectories
+# Print the top 10 subdirectories with sizes
 echo "Top 10 Subdirectories:"
-echo "$subdirectories" | head -n 10
+echo "$subdirectories" | head -n 10 | awk '{print $1 "\t" $2}'
